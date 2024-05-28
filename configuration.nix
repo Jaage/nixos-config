3bf8a5b759b6fef1428ffcd55ff920598ad6f24d
jjh <jordanjhubbard@gmail.com>
2024-05-28 07:33:27 -0700

    diff --git a/configuration.nix b/configuration.nix index 1bca4ab..a930187 100644 --- a/configuration.nix +++ b/configuration.nix @@ -80 +80 @@ -  services.desktopManager.plasma5.enable = true; +  services.xservice.desktopManager.plasma5.enable = true;

--- configuration.nix
+++ configuration.nix
@@ -77,7 +77,7 @@
   # Enable the KDE Plasma Desktop Environment.
   services.xservice.displayManager.defaultSession = "plasma";
   services.xservice.displayManager.sddm.wayland.enable = true;
-  services.desktopManager.plasma5.enable = true;
+  services.xservice.desktopManager.plasma5.enable = true;
 
 #  services.displayManager.defaultSession = "plasma";
 #  services.displayManager.sddm.wayland.enable = true;

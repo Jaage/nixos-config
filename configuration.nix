1f887c056324ded6ea96aa8a70b315db1c15f393
jjh <jordanjhubbard@gmail.com>
2024-05-28 08:28:47 -0700

    diff --git a/configuration.nix b/configuration.nix index 3dbc2f7..8c2139f 100644 --- a/configuration.nix +++ b/configuration.nix @@ -27,0 +28 @@ +  boot.kernelParams = [ "nvidia-drm.modeset=1" "nvidia-drm.fbdev=1" ];

--- configuration.nix
+++ configuration.nix
@@ -25,6 +25,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelParams = [ "nvidia-drm.modeset=1" "nvidia-drm.fbdev=1" ];
 
   # Storage Optimization
   nix.gc = {

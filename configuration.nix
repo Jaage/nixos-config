9e16f7ae01169078a67ba976ea0828edf6d2390c
jjh <jordanjhubbard@gmail.com>
2024-05-28 17:09:08 -0700

    diff --git a/configuration.nix b/configuration.nix index 54b04ea..65c8a9a 100644 --- a/configuration.nix +++ b/configuration.nix @@ -7,8 +7,3 @@ -#let -#  stylix = import (pkgs.fetchFromGitHub { -#    owner = "danth"; -#    repo = "stylix"; -#    rev = "290c8aef476ce98fff9cefc059284429d561a085"; -#    sha256 = "sha256-s9Tyj5pEivl/AsvrpkUkfR1Iu3zHfXpviPfe4HbPJ5I="; -#  }); -#in +let +  luaRc = (import ./luaRc.nix); +in @@ -215,68 +210,4 @@ -      ''; -      luaRc = '' -        require('fidget').setup {} -        local lsp_zero = require('lsp-zero') -        lsp_zero.on_attach(function(client, bufnr) -          lsp_zero.default_keymaps({buffer = bufnr}) -        end) - -        local cmp = require('cmp') -        cmp.setup { -          snippet = { -            expand = function(args) -              require('luasnip').lsp_expand(args.body) -            end, -          }, -          mapping = { -            ['<C-p>'] = cmp.mapping.select_prev_item(), -            ['<C-n>'] = cmp.mapping.select_next_item(), -            ['<C-space>'] = cmp.mapping.complete(), -            ['<C-e>'] = cmp.mapping.close(), -            ['<tab>'] = cmp.mapping.confirm { select = true }, -          }, -          sources = cmp.config.sources({ -            { name = 'nvim-lsp' }, -            { name = 'luasnip' }, -          }), -        } - -        local lsp_mappings = { -          { 'gD', vim.lsp.buf.declaration }, -          { 'gd', vim.lsp.buf.definition }, -          { 'gi', vim.lsp.buf.implementation }, -          { 'gr', vim.lsp.buf.references }, -          { '[d', vim.diagnostic.goto_prev }, -          { ']d', vim.diagnostic.goto_next }, -          { ' ' , vim.lsp.buf.hover }, -          { ' s', vim.lsp.buf.signature_help }, -          { ' d', vim.diagnostic.open_float }, -          { ' q', vim.diagnostic.setloclist }, -          { '\\r', vim.lsp.buf.rename }, -          { '\\a', vim.lsp.buf.code_action }, -        } -        for i, map in pairs(lsp_mappings) do -          vim.keymap.set('n', map[1], function() map[2]() end) -        end -        vim.keymap.set('x', '\\a', function() vim.lsp.buf.code_action() end) - -        local caps = vim.tbl_deep_extend( -          'force', -          vim.lsp.protocol.make_client_capabilities(), -          require('cmp_nvim_lsp').default_capabilities(), -          { workspace = { didChangeWatchedFiles = { dynamicRegistration = true } } } -        ); - -        local lsp_path = vim.env.NIL_PATH or 'target/debug/nil' -        require('lspconfig').nil_ls.setup { -          autostart = true, -          capabilities = caps, -          cmd = { lsp_path }, -          settings = { -            ['nil'] = { -              testSetting = 42, -              formatting = { -                command = { "nixpkgs-fmt" }, -              }, -            }, -          }, -        } + +        lua <<EOF + +        EOF @@ -295 +226 @@ -#          onedark-nvim +          onedark-nvim

--- configuration.nix
+++ configuration.nix
@@ -4,14 +4,9 @@
 
 { config, pkgs, lib, inputs, ... }:
 
-#let
-#  stylix = import (pkgs.fetchFromGitHub {
-#    owner = "danth";
-#    repo = "stylix";
-#    rev = "290c8aef476ce98fff9cefc059284429d561a085"; 
-#    sha256 = "sha256-s9Tyj5pEivl/AsvrpkUkfR1Iu3zHfXpviPfe4HbPJ5I=";
-#  });
-#in
+let
+  luaRc = (import ./luaRc.nix);
+in
 {
   imports =
     [ # Include the results of the hardware scan.
@@ -212,74 +207,10 @@
         set expandtab
         set tabstop=2
         set shiftwidth=2
-      '';
-      luaRc = ''
-        require('fidget').setup {}
-        local lsp_zero = require('lsp-zero')
-        lsp_zero.on_attach(function(client, bufnr)
-          lsp_zero.default_keymaps({buffer = bufnr})
-        end)
 
-        local cmp = require('cmp')
-        cmp.setup {
-          snippet = {
-            expand = function(args)
-              require('luasnip').lsp_expand(args.body)
-            end,
-          },
-          mapping = {
-            ['<C-p>'] = cmp.mapping.select_prev_item(),
-            ['<C-n>'] = cmp.mapping.select_next_item(),
-            ['<C-space>'] = cmp.mapping.complete(),
-            ['<C-e>'] = cmp.mapping.close(),
-            ['<tab>'] = cmp.mapping.confirm { select = true },
-          },
-          sources = cmp.config.sources({
-            { name = 'nvim-lsp' },
-            { name = 'luasnip' },
-          }),
-        }
-
-        local lsp_mappings = {
-          { 'gD', vim.lsp.buf.declaration },
-          { 'gd', vim.lsp.buf.definition },
-          { 'gi', vim.lsp.buf.implementation },
-          { 'gr', vim.lsp.buf.references },
-          { '[d', vim.diagnostic.goto_prev },
-          { ']d', vim.diagnostic.goto_next },
-          { ' ' , vim.lsp.buf.hover },
-          { ' s', vim.lsp.buf.signature_help },
-          { ' d', vim.diagnostic.open_float },
-          { ' q', vim.diagnostic.setloclist },
-          { '\\r', vim.lsp.buf.rename },
-          { '\\a', vim.lsp.buf.code_action },
-        }
-        for i, map in pairs(lsp_mappings) do
-          vim.keymap.set('n', map[1], function() map[2]() end)
-        end
-        vim.keymap.set('x', '\\a', function() vim.lsp.buf.code_action() end) 
-
-        local caps = vim.tbl_deep_extend(
-          'force',
-          vim.lsp.protocol.make_client_capabilities(),
-          require('cmp_nvim_lsp').default_capabilities(),
-          { workspace = { didChangeWatchedFiles = { dynamicRegistration = true } } }
-        );
-
-        local lsp_path = vim.env.NIL_PATH or 'target/debug/nil'
-        require('lspconfig').nil_ls.setup {
-          autostart = true,
-          capabilities = caps,
-          cmd = { lsp_path },
-          settings = {
-            ['nil'] = {
-              testSetting = 42,
-              formatting = {
-                command = { "nixpkgs-fmt" },
-              },
-            },
-          },
-        }
+        lua <<EOF
+        
+        EOF
       '';
       packages.myVimPackage = with pkgs.vimPlugins; {
         start = [
@@ -292,7 +223,7 @@
           cmp_luasnip
           cmp-nvim-lsp
 #          harpoon
-#          onedark-nvim
+          onedark-nvim
         ];
       };
     };

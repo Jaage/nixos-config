6753f7695406a7fb1f287fc2cb700c1338744573
jjh <jordanjhubbard@gmail.com>
2024-06-10 21:06:00 -0700

    No changes

--- configuration.nix
+++ configuration.nix
@@ -140,9 +140,6 @@ in
     #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.jjh = {
     isNormalUser = true;
